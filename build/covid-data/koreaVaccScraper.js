6411df376300e695ef8ec40b965c12b2fe48ea3a
Julius Oh <juliusoh@gmail.com>
2022-01-12 20:31:36 -0800

    Delete build directory

--- build/covid-data/koreaVaccScraper.js
+++
@@ -1,9 +0,0 @@
-// const colNames = ['col 1', 'col 2', 'col 3']
-// const finalArray = []
-// trs.forEach(tr => {
-//     const rowObj = {}
-//     tr.tds.forEach((td, idx) => {
-//         rowObj[colNames[idx]] = td;
-//     })
-//     finalArray.push(rowObj)
-// })
